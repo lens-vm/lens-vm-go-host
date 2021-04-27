51ac8d06f14b88cae11e6c5fc2bd09a3e1d37a9e
John-Alan Simmons <simmons.johnalan@gmail.com>
2021-04-27 17:03:10 -0400

    Started dependancy graph system

--- dependancy.go
+++ dependancy.go
@@ -2,12 +2,32 @@ package lensvm
 
 import (
 	"github.com/lens-vm/gogl"
+	"github.com/lens-vm/gogl/dfs"
+	"github.com/lens-vm/gogl/graph/al"
 )
 
 type dependancyNode struct{}
 
 type dependancyGraph struct {
-	graph gogl.Graph
+	graph gogl.MutableLabeledGraph
+}
+
+func newDependancyGraph() *dependancyGraph {
+	graph := gogl.Spec().
+		// The graph should be mutable. Default is immutable.
+		Mutable().
+		// The graph should have directed edges (arcs). Default is undirected.
+		Directed().
+		// The graph's edges are plain - no labels, weights, etc. This is the default.
+		Labeled().
+		// No loops or parallel edges. This is the default.
+		SimpleGraph().
+		// al.G picks and returns an adjacency list-based graph, based on the spec.
+		Create(al.G).
+	// The builder always returns a Graph; type assert to get access to add/remove methods.
+	(gogl.MutableLabeledGraph)
+
+	return &dependancyGraph{graph}
 }
 
 // Both Module and Lens files define an import
@@ -26,3 +46,28 @@ type dependancyGraph struct {
 // A ──┤				   ├──> I
 //     └─> C ──> E ─┬─> F ─┘
 //                  └─> G
+
+func (vm *VM) makeDependancyGraph() error {
+	vm.dgraph = newDependancyGraph()
+	for id, mod := range vm.moduleImports {
+		for lens, modDep := range mod.dependancies {
+			vm.dgraph.graph.AddEdges(gogl.NewLabeledEdge(id, modDep.id, lens))
+		}
+	}
+	return nil
+}
+
+func (d dependancyGraph) SortOrder(root string) ([]string, error) {
+	var sortV []gogl.Vertex
+	sortV, err := dfs.Toposort(d.graph, root)
+	if err != nil {
+		return nil, err
+	}
+
+	sortS := make([]string, len(sortV))
+	for i, v := range sortV {
+		sortS[i] = v.(string)
+	}
+
+	return sortS, nil
+}
